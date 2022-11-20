f976b30534391c11204d8d20841e1b5c1872ef30
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2022-11-20 20:45:14 +0200

    order for a static line at 6:45 tomorrow

--- bot.py
+++ bot.py
@@ -3,6 +3,30 @@ from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 import os
 import re
+import requests
+import datetime
+
+test_order = {
+    "FirstName": "משה",
+    "LastName": "כהן",
+    "Phone": "0542345732",
+    "Mobile": "0542345732",
+    "TripID": "50795116",
+    "Clerk": "865",
+    "HostName": "WebSite",
+    "CityID": 0,
+    "StationID": "806601",
+    "LineID": 37848,
+    "DDate": "11/11/2022",
+    "DtTime": "06:45",
+    "NumOfPax": "1",
+    "Remarks": "Remark",
+    "StatusID": 1,
+    "Boarded": 1,
+    "Destination": "",
+    "DStationID": "0"
+}
+
 
 # set up the introductory statement for the bot when the /start command is invoked
 def start(update, context):
@@ -18,8 +42,26 @@ def details(update, context):
 def order(update, context):
     input_details = update.message.text
     print(input_details)
-    update.message.reply_text("Your order is: " + input_details)
+
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    test_order["DDate"] = tomorrow.strftime("%d/%m/%Y")
+    print(test_order)
+
+    result = _run_order(test_order)
+    if result and result["d"]:
+        update.message.reply_text("Your order id is: " + result["d"])
+    else:
+        update.message.reply_text("Your order failed: " + result)
+    
     return
+
+def _run_order(order):
+    url = 'https://www.nateevexpress.com/OnCall/NateevWebService.asmx/AddNewOnCall'
+    x = requests.post(url, json = order)
+    print(x.json())
+
+    return x.json()
 
 
 telegram_bot_token = os.environ['TGRM_TKN']
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 import os
 os.environ['TGRM_TKN'] = "TEST"
 
-from bot import _transform_link
-print(_transform_link("תיק 4000 קרס? כך נלחם העליון בפייק ניוז על תיקי נתניהוhttps://www.themarker.com/law/2022-08-28/ty-article/.premium/00000182-dfaa-d9c0-a3d3-ffba90f60000?utm_source=App_Share&utm_medium=Android_Native&utm_campaign=Share"))
-print(_transform_link("https://www.themarker.com/law/2022-08-28/ty-article/.premium/00000182-dfaa-d9c0-a3d3-ffba90f60000?utm_source=App_Share&utm_medium=Android_Native&utm_campaign=Share"))
+from bot import _run_order
+order = {
+    "FirstName": "ddd",
+    "LastName": "fff",
+    "Phone": "0542277222",
+    "Mobile": "0542277222",
+    "TripID": "50795116",
+    "Clerk": "865",
+    "HostName": "WebSite",
+    "CityID": 0,
+    "StationID": "806601",
+    "LineID": 37848,
+    "DDate": "11/11/2022",
+    "DtTime": "06:45",
+    "NumOfPax": "1",
+    "Remarks": "Remark",
+    "StatusID": 1,
+    "Boarded": 1,
+    "Destination": "",
+    "DStationID": "0"
+}
+
+print(_run_order(order))
